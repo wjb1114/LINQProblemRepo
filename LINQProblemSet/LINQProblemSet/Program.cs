3bc345420a3e29e44cc88639a6ced696d11754b8
wjb1114 <kbd1114@gmail.com>
2019-07-30 10:55:58 -0500

    Finished last method, changed all to take input and return values.

--- LINQProblemSet/LINQProblemSet/Program.cs
+++ LINQProblemSet/LINQProblemSet/Program.cs
@@ -10,39 +10,6 @@ namespace LINQProblemSet
     {
         static void Main(string[] args)
         {
-            //GetSubstrings(); // done
-            //RemoveDuplicates(); // done
-            //AverageGrades(); // done
-            //GetLetterFrequency();
-
-            Console.ReadKey();
-        }
-
-        static void GetSubstrings()
-        {
-            // 1. Using LINQ, write a function that returns all words that contain the substring “th” from a list.
-            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
-            IEnumerable<string> results = words.Where(w => w.Contains("th"));
-            foreach(string word in results)
-            {
-                Console.WriteLine(word);
-            }
-        }
-
-        static void RemoveDuplicates()
-        {
-            // 2.Using LINQ, write a function that takes in a list of strings and returns a copy of the list without duplicates.
-            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
-            IEnumerable<string> results = names.Distinct();
-            foreach (string word in results)
-            {
-                Console.WriteLine(word);
-            }
-        }
-
-        static void AverageGrades()
-        {
-            // 3.Using LINQ, write a function that calculates the class grade average after dropping the lowest grade for each student.The function should take in a list of strings of grades (e.g., one string might be "90,100,82,89,55"), drops the lowest grade from each string, averages the rest of the grades from that string, then averages the averages.
             List<string> classGrades = new List<string>()
             {
                 "80,100,92,89,65",
@@ -50,6 +17,50 @@ namespace LINQProblemSet
                 "73,88,83,99,64",
                 "98,100,66,74,55"
             };
+
+            Console.WriteLine(GetSubstrings(new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" }, "th"));
+            Console.WriteLine(RemoveDuplicates(new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" }));
+            Console.WriteLine(AverageGrades(classGrades));
+            Console.WriteLine(GetLetterFrequency("Terrill"));
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+
+        static string GetSubstrings(List<string> words, string delimiter)
+        {
+            // 1. Using LINQ, write a function that returns all words that contain the substring “th” from a list.
+            IEnumerable<string> results = words.Where(w => w.Contains(delimiter));
+            string result = "";
+            foreach(string word in results)
+            {
+                if (result.Length > 0)
+                {
+                    result += ", ";
+                }
+                result += word;
+            }
+            return result;
+        }
+
+        static string RemoveDuplicates(List<string> names)
+        {
+            // 2.Using LINQ, write a function that takes in a list of strings and returns a copy of the list without duplicates.
+            IEnumerable<string> results = names.Distinct();
+            string result = "";
+            foreach (string word in results)
+            {
+                if (result.Length > 0)
+                {
+                    result += ", ";
+                }
+                result += word;
+            }
+            return result;
+        }
+
+        static double AverageGrades(List<string> classGrades)
+        {
+            // 3.Using LINQ, write a function that calculates the class grade average after dropping the lowest grade for each student.The function should take in a list of strings of grades (e.g., one string might be "90,100,82,89,55"), drops the lowest grade from each string, averages the rest of the grades from that string, then averages the averages.
             List<double> individualAverages = new List<double>();
             foreach (string initialValue in classGrades)
             {
@@ -58,13 +69,21 @@ namespace LINQProblemSet
                 individualAverages.Add(updated);
             }
             var totalAverage = individualAverages.Average();
-            Console.WriteLine(totalAverage);
+            return totalAverage;
             // Expected output: 86.125
         }
 
-        static void GetLetterFrequency()
+        static string GetLetterFrequency(string word)
         {
             // 4.Write a function that takes in a string of letters(i.e. “Terrill”) and returns an alphabetically ordered string corresponding to the letter frequency(i.e. "E1I1L2R2T1")
+            var characters = word.ToUpper().ToCharArray().OrderBy(c => c).GroupBy(c => c);
+            string result = "";
+            foreach (var c in characters)
+            {
+                result += c.Key;
+                result += c.Count();
+            }
+            return result;
         }
     }
 }
